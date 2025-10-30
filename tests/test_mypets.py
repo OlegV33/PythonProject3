384953af49a9726d33e06be00fba44f330f4287a
OlegV33 <fin33v@mail.ru>
2025-10-30 19:18:50 +0700

    Update test_mypets.py

--- tests/test_mypets.py
+++ tests/test_mypets.py
@@ -1,11 +1,12 @@
 import pytest
-from settings import email, password
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+email = "alexv9999@mail.ru"
+password = "qwaszx1"
 
 @pytest.fixture
 def driver():
